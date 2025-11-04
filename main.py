48d07f56db060edbdbd5b54bc01cdf195a488910
NEFORCEO <pirogartem919@gmail.com>
2025-11-04 14:16:56 +0700

    UPDATE Project

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 from client.config.config import app_name, host, port, reload
 from client.run import start_app
-
+from routers.router import router
 
 app = FastAPI(lifespan=start_app)
-
+app.include_router(router=router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app=app_name,host=host, port=post, reload=reload)
+    uvicorn.run(app=app_name,host=host, port=port, reload=reload)
