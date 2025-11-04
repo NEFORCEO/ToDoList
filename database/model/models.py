791c6fa6c3e97722f7a58716706efc61e8bf795b
NEFORCEO <pirogartem919@gmail.com>
2025-11-04 15:21:40 +0700

    UPDATE MODELS

--- database/model/models.py
+++ database/model/models.py
@@ -11,6 +11,5 @@ async def init_db():
 async def get_session() -> AsyncSession:
     async with session_create() as session:
         yield session
-    
-SessionDep = Annotated[AsyncSession, Depends(get_session)]
+
     
